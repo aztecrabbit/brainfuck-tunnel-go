2ff9947d973cbf7df224dffaf74adb501026debc
Aztec Rabbit <ars.xda@gmail.com>
2020-01-19 02:23:07 +0700

    ...

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"os"
+    "os/signal"
+	"fmt"
+	"sync"
+    "syscall"
+	"strconv"
+
+	"github.com/aztecrabbit/liblog"
+	"github.com/aztecrabbit/libproxyrotator"
+	"github.com/aztecrabbit/brainfuck-tunnel/src/sshclient"
+)
+
+const (
+	appName = "Brainfuck Tunnel"
+	appVersionName = "Go"
+	appVersionCode = "200117"
+
+	copyrightYear = "2020"
+	copyrightAuthor = "Aztec Rabbit"
+)
+
+func main() {
+	SetupCloseHandler()
+
+	liblog.LogColor(
+		fmt.Sprintf("%s [%s Version. %s] \n(c) %s %s. \n",
+			appName, appVersionName, appVersionCode, copyrightYear, copyrightAuthor),
+		liblog.Colors["G1"],
+	)
+
+	var wg sync.WaitGroup
+
+	threads := 6
+	channel := make(chan bool, threads)
+
+	ProxyRotator := new(libproxyrotator.ProxyRotator)
+	ProxyRotator.Port = "3080"
+
+	if len(os.Args) > 1 {
+		ProxyRotator.Port = os.Args[1]
+	}
+
+	for i := 1; i <= threads; i++ {
+		wg.Add(1)
+
+		ProxyRotatorPort, err := strconv.Atoi(ProxyRotator.Port)
+		if err != nil {
+			panic(err)
+		}
+
+		SshClient := new(sshclient.SshClient)
+		// SshClient.Host = "m.sg1.ssh.speedssh.com"
+		// SshClient.Host = "157.245.62.248"
+		// SshClient.Port = "22"
+		// SshClient.Username = "speedssh.com-aztecrabbit"
+		SshClient.Host = "103.253.27.56"
+		SshClient.Port = "80"
+		SshClient.Username = "aztecrabbit"
+		SshClient.Password = "aztecrabbit"
+		SshClient.InjectHost = "0.0.0.0"
+		SshClient.InjectPort = "8989"
+		SshClient.ListenPort = strconv.Itoa(ProxyRotatorPort + i)
+		SshClient.Loop = true
+
+		ProxyRotator.Proxies = append(ProxyRotator.Proxies, "0.0.0.0:" + SshClient.ListenPort)
+
+		go SshClient.Start(&wg, channel)
+	}
+
+	ProxyRotator.Start()
+
+	for i := 0; i < threads; i++ {
+		channel <- true
+	}
+
+	wg.Wait()
+}
+
+func SetupCloseHandler() {
+    channelSignal := make(chan os.Signal, 2)
+    signal.Notify(channelSignal, os.Interrupt, syscall.SIGTERM)
+    go func() {
+        <- channelSignal
+        liblog.LogInfo(
+        	"Keyboard Interrupt\n\n" +
+        		"|   Ctrl-C again if not exiting automaticly\n" +
+        		"|   Please wait...\n|\n",
+
+        	"INFO", liblog.Colors["R1"],
+        )
+        sshclient.Stop()
+        os.Exit(0)
+    }()
+}
---
+++ src/sshclient/sshclient.go
@@ -0,0 +1,111 @@
+package sshclient
+
+import (
+	"fmt"
+	"os/exec"
+	"sync"
+	"time"
+	"bufio"
+	"strings"
+	"github.com/aztecrabbit/liblog"
+)
+
+var (
+	Loop bool = true
+)
+
+func Stop() {
+	Loop = false
+}
+
+type SshClient struct {
+	Host string
+	Port string
+	Username string
+	Password string
+	InjectHost string
+	InjectPort string
+	ListenPort string
+	Loop bool
+}
+
+func (s *SshClient) LogColor(message string, color string) {
+	if s.Loop {
+		liblog.LogInfo(message, s.ListenPort, color)
+	}
+}
+
+func (s *SshClient) Log(message string) {
+	s.LogColor(message, liblog.Colors["G1"])
+}
+
+func (s *SshClient) Stop() {
+	s.Loop = false
+}
+
+func (s *SshClient) Start(wg *sync.WaitGroup, channel chan bool) {
+	defer wg.Done()
+
+	<- channel
+
+	s.Log(fmt.Sprintf("Connecting to %s port %s", s.Host, s.Port))
+
+	for Loop && s.Loop {
+		command := exec.Command(
+			"dash", "-c", fmt.Sprintf(
+				"sshpass -p '%s' ssh -v %s -p %s -l '%s' " +
+					"-o StrictHostKeyChecking=no " +
+					"-o UserKnownHostsFile=/dev/null " +
+					"-o ProxyCommand='nc -X CONNECT -x %s:%s %%h %%p' " +
+					"-CND %s ",
+				s.Password,
+				s.Host,
+				s.Port,
+				s.Username,
+				s.InjectHost,
+				s.InjectPort,
+				s.ListenPort,
+			),
+		)
+
+		stderr, err := command.StderrPipe()
+		if err != nil {
+			panic(err)
+		}
+
+		scanner := bufio.NewScanner(stderr)
+		go func() {
+			var line string
+			for Loop && s.Loop && scanner.Scan() {
+				line = scanner.Text()
+
+				if strings.Contains(line, "debug1: pledge: ") {
+					s.LogColor("Connected", liblog.Colors["Y1"])
+
+				} else if strings.Contains(line, "Permission denied") {
+					s.LogColor("Access Denied", liblog.Colors["R1"])
+					s.Stop()
+
+				} else if strings.Contains(line, "Connection closed") {
+					s.LogColor("Connection closed", liblog.Colors["R1"])
+
+				} else if strings.Contains(line, "Address already in use") {
+					s.LogColor("Port used by another programs", liblog.Colors["R1"])
+					s.Stop()
+
+				} else {
+					// s.LogColor(line, liblog.Colors["G2"])
+
+				}
+			}
+		}()
+
+		command.Start()
+		command.Wait()
+
+		time.Sleep(200 * time.Millisecond)
+
+		s.Log("Reconnecting")
+	}
+
+}
